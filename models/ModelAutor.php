f817f59bae41431f1ffef514cc5029f8afb1fc1c
Ivan Zarkovic <ivanzarkovic@yahoo.com>
2021-06-11 11:07:31 +0200

    Implement business logic

--- models/ModelAutor.php
+++ models/ModelAutor.php
@@ -27,15 +27,32 @@ class Autor {
     }
     
     public static function dohvatiAutora($korisnicko_ime){
-        // TO-DO
+        $konekcija = DB::getInstanca();
+        $rezultat = $konekcija->query("SELECT * FROM autor WHERE korisnicko_ime='$korisnicko_ime'");     
+        $autor = $rezultat->fetch();
+        if ($autor!=NULL) {
+            return new Autor($autor['korisnicko_ime'], $autor['lozinka'], $autor['ime'], $autor['prezime'],$autor['admin']);
+        } else {
+            return FALSE;
+        }
     }
     
     public function ispravanPassword($lozinka){
-        // TO-DO
+        if ($this->lozinka == $lozinka) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
     }
     
     public static function dohvatiAutore(){
-        // TO-DO
+        $konekcija = DB::getInstanca();
+        $rezultat = $konekcija->query("SELECT * FROM autor");     
+        $niz = [];
+        foreach($rezultat->fetchAll() as $autor){
+         	$niz[] = new Autor($autor['korisnicko_ime'], $autor['lozinka'], $autor['ime'], $autor['prezime'], $autor['admin']);
+        }
+        return $niz;
     }
     
 }
